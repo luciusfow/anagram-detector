b70689e5a71a4c8e092f7e79af2582f59f14f744
Lucius Fowler <luciusfow@gmail.com>
2021-01-07 21:24:52 -0500

    commit tree a8332150bdb0c97e0212f09cc925483f82a63157

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :words
+
+    def initialize(words)
+        @words = words
+    end 
+#     def character
+#         @words.split("").sort
+#     end
+#     def characters_array(words)
+#         words.split("").sort
+#     end
+#     def match(something)
+#         something.select do |words|
+#             characters_array(words)== character
+#         end
+#     end
+def match(all_my_words)
+    all_my_words.select do |words|
+        @words.split("").sort == words.split("").sort
+    end
+end
+end
